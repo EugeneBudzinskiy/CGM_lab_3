64b1e5a04e1077adde9cadabf3654d3486443f43
Eugene <eugene.ususual@gmail.com>
2020-11-24 11:12:32 +0200

    Initial commit

---
+++ CellFinder.py
@@ -0,0 +1,54 @@
+class CellFinder:
+
+    """ 'Gift wrapping' algorithm, also known as 'Jarvis march' """
+
+    def __init__(self):
+        self.search_poll = []                                   # List of point which include in cell search
+        self.cell = []                                          # Result list of coords of cell
+        self.n = 0                                              # Length of input coords list
+
+    def init_search_poll(self, coord_array):                    # Initialization of 'n' and 'search_poll'
+        self.n = len(coord_array)                               # Write length of input coords list in 'n'
+        self.search_poll = list(range(self.n))                  # Init 'search_poll' as indexes of 'coord_array'
+
+    def find_most_left_point(self, coord_array):                # Finding the most-left point in 'coord_array'
+        for i in range(1, self.n):                              # Loop throw all indexes of 'coord_array'
+            first_coord = coord_array[self.search_poll[0]]      # Get the current the most-left point
+            current_coord = coord_array[self.search_poll[i]]    # Get the current point for match
+
+            if first_coord[1] > current_coord[1]:               # If current point is 'lefter' than current most-left
+                self.search_poll[0], self.search_poll[i] = \
+                    self.search_poll[i], self.search_poll[0]    # Swap the current point and current most-left point
+
+        self.cell = [self.search_poll[0]]                       # Set the real most-left point in head of result cell
+        self.search_poll.pop(0)                                 # Delete point which we already processed
+        self.search_poll.append(self.cell[0])                   # Add processed point in to end of 'search_poll'
+
+    @staticmethod
+    def rotate(a, b, c):                                        # "Rotate" stuff using Linear Algebra (scalar dot)
+        return (b[0] - a[0]) * (c[1] - b[1]) - (b[1] - a[1]) * (c[0] - b[0])
+
+    def process(self, coord_array):                             # Main loop
+        self.init_search_poll(coord_array)                      # Init of 'search_poll'
+        self.find_most_left_point(coord_array)                  # Finding the most-left point
+
+        flag = True                                             # Set the end-flag
+        while flag:                                             # While loop
+            right = 0                                           # Init right as 0
+
+            for i in range(1, len(self.search_poll)):           # For throw all indexes in 'search_poll'
+                last_p = coord_array[self.cell[-1]]             # Choose the last point for match
+                right_p = coord_array[self.search_poll[right]]  # Choose the right point for match
+                cur_p = coord_array[self.search_poll[i]]        # Choose the current point for match
+
+                if self.rotate(last_p, right_p, cur_p) < 0:     # Check position of point relate to last_point
+                    right = i                                   # Set current index as 'right'
+
+            if self.search_poll[right] == self.cell[0]:         # If current right point same as first point of cell
+                flag = False                                    # Stop the while loop
+
+            else:                                               # Otherwise
+                self.cell.append(self.search_poll[right])       # Append this "right" point to result cell
+                self.search_poll.pop(right)                     # And delete this point from 'search_poll'
+
+        return self.cell                                        # Return result cell
---
+++ main.py
@@ -0,0 +1,61 @@
+from PIL import Image, ImageDraw
+from numpy import ones, multiply
+
+from CellFinder import CellFinder                        # Import Class of Cell Finder algorithm
+
+
+def main():
+    filename = 'DS1.txt'                                 # Name of dataset file
+    result_filename = 'result.png'                       # Name of result file
+    canvas_size = (540, 960)                             # Canvas size of image
+
+    cell_finder = CellFinder()                           # Init of CellFinder
+
+    with open(filename) as file:                         # Work with file
+        canvas_size_ext = canvas_size + tuple([3])       # Add RGB-mask part
+        mask = ones(canvas_size_ext, dtype='uint8')      # Create numpy array fill with ones
+
+        file_array = file.read()                         # Read all data from file in to array
+        file_array = file_array[:-1]                     # Delete last '\n' from array
+        file_array = file_array.split('\n')              # Split coordination by '\n'
+
+        coords = [x.split(' ') for x in file_array]      # Split coord from 1d array to 2 2d array
+        coords = [[int(y) for y in x] for x in coords]   # Convert str coord to int coord
+
+        for coord_x, coord_y in coords:                  # Work with coordination's
+            mask[coord_x, coord_y] = [0, 0, 0]           # Change color of right pixels to black
+
+        image = multiply(mask, 255)                      # Scale data from [0, 1] to [0, 255]
+        image = Image.fromarray(image)                   # Create image from data array
+        image = image.transpose(Image.FLIP_TOP_BOTTOM)   # Flip the image
+
+        image.show()                                     # Show result without cell
+        image.save(result_filename)                      # Save the result image
+
+        image = image.transpose(Image.FLIP_TOP_BOTTOM)   # Flip back the image for cell
+        draw = ImageDraw.Draw(image)                     # Create a drawing tool
+
+        cell_result = cell_finder.process(coords)        # Run the cell finder
+        ln = len(cell_result)                            # Get len of result cell list
+
+        for i in range(ln):                              # Run throw all point of cell
+            start_line = cell_result[i]                  # Init the start line position in 'coords'
+
+            if i == ln - 1:                              # if last point of cell
+                end_line = cell_result[0]                # Close the cell
+            else:
+                end_line = cell_result[i + 1]            # Choose the next point of cell
+
+            start_x, start_y = coords[start_line]        # Convert position in 'coords' in to real coords
+            end_x, end_y = coords[end_line]              # Same for end of line
+
+            draw.line((start_y, start_x, end_y, end_x),  # Draw the line where is need
+                      fill=(0, 191, 255), width=2)
+
+        image = image.transpose(Image.FLIP_TOP_BOTTOM)  # Flip the image
+        image.show()                                    # Show the result with cell
+        image.save('cell_' + result_filename)           # Save the CELL result image
+
+
+if __name__ == '__main__':
+    main()
